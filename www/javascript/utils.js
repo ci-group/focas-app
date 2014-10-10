a5a8c7fe9db510ce09c6a77770a10499ad65d9c1
Berend Weel <b.weel@vu.nl>
2014-10-10 14:09:01 +0200

    no message

--- www/javascript/utils.js
+++ www/javascript/utils.js
@@ -9,40 +9,16 @@
  In above code variable "tech" will have "jquery" as value and "blog" variable's will be "jquerybyexample".
 */
 
-function getURLParameter(sParam, sString)
-{
-    var sPageURL = sString.split('?')[1];
-//    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++)
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam)
-        {
-            return sParameterName[1];
-        }
+var Utils = {
+    embedVideo: function(file) {
+        var text = "";
+        text += "<video id='video-"+file+"' width='100%' controls autoplay>";
+        text += "<source src='video/"+file+"' type='video/mp4'>";
+        text += "<object data='video/"+file+"'>";
+        text += "<embed src='video/"+file+"'>";
+        text += "</object>";
+        text += "</video>";
+
+        $("#video-embed").html(text);
     }
-}
-
-
-function embedVideo(file) {
-    var text = "";
-    text += "<video id='video-"+file+"' width='100%' controls autoplay>";
-    text += "<source src='video/"+file+"' type='video/mp4'>";
-    text += "<object data='video/"+file+"'>";
-    text += "<embed src='video/"+file+"'>";
-    text += "</object>";
-    text += "</video>";
-    
-    $("#video-embed").html(text);
-    
-    //$("video-"+file).bind('ended', function(){
-    //    revertImage(number);
-    //                       });
-}
-
-function revertImage(number){
-    var text = "<img src='images/Robot_"+number+".jpg' width='320' height='240' onclick='embedVideo("+number+")'>";
-    
-    document.getElementById('robot'+number).innerHTML = text;
 }
